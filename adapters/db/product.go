7201feb1012b841a247cc23bd39ad45b5dfa3780
Christian Seidler <ch.cseidler@gmail.com>
2023-05-17 22:43:04 -0300

    Get method in adapter DB

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -12,5 +12,14 @@ type ProductDb struct {
 }
 
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
-
+	var product application.Product
+	stmt, err := p.db.Prepare("select id, name, price, status from products where id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.Id, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
 }
